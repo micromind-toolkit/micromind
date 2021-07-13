0c85333d97fd7a5567aab1ff34b4885b50afc7e3
Francesco Paissan <me@francescopaissan.it>
2021-07-13 17:13:58 +0200

    Add torchinfo [not working[

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ from torchinfo import summary
 
 
 if __name__ == "__main__":
-    pn = PhiNet(res=128, B0=7, alpha=0.2, beta=1, t_zero=6, squeeze_excite=True, h_swish=True)
+    pn = PhiNet(res=128, B0=7, alpha=0.2, beta=1, t_zero=6, squeeze_excite=True, h_swish=True).cuda()
     
     rand_x = Variable(torch.Tensor(np.random.rand(1, 3, 128, 128)))
 
     print(pn.forward(rand_x).shape)
 
-    summary(pn, input_shape=(1, 3, 128, 128))
+    summary(pn, input_size=(1, 3, 128, 128), col_names = ["input_size", "output_size", "num_params", "kernel_size", "mult_adds"], verbose=2)
