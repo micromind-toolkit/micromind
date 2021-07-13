62ff6e55cc3270c07eebcaeb3c72d99b0c6a52f2
Francesco Paissan <me@francescopaissan.it>
2021-07-13 17:17:27 +0200

    Maybe torchinfo working

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ from torchinfo import summary
 
 
 if __name__ == "__main__":
-    pn = PhiNet(res=128, B0=7, alpha=0.2, beta=1, t_zero=6, squeeze_excite=True, h_swish=True).cuda()
+    pn = PhiNet(res=128, B0=7, alpha=0.2, beta=1, t_zero=6, squeeze_excite=True, h_swish=True).to("cpu")
     
     rand_x = Variable(torch.Tensor(np.random.rand(1, 3, 128, 128)))
 
     print(pn.forward(rand_x).shape)
 
-    summary(pn, input_size=(1, 3, 128, 128), col_names = ["input_size", "output_size", "num_params", "kernel_size", "mult_adds"], verbose=2)
+    var = Variable(torch.Tensor(np.random.rand(1, 3, 96, 96))).to("cpu")
+
+    summary(pn, input_data=var, col_names = ["input_size", "output_size", "num_params", "kernel_size", "mult_adds"], verbose=2, device="cpu")
