685addaf33be5fe2d3494cb7c51af718ad0185bc
Francesco Paissan <me@francescopaissan.it>
2021-07-26 11:21:44 +0200

    Mods to cifar10

--- __main__.py
+++ __main__.py
@@ -8,6 +8,7 @@ import torch
 
 import click
 
+
 @click.command()
 @click.argument('exp_id', type=str)
 @click.argument('data_path', type=str)
@@ -31,11 +32,12 @@ def main(exp_id, data_path):
                             callbacks=[checkpoint_callback])
 
         trainer.fit(model=mod, datamodule=data_module)
+        # trainer.test(datamodule=data_module, verbose=1)
     
     else:
-        pn = PhiNet(res=32, B0=7, alpha=0.2, beta=1, t_zero=5, squeeze_excite=True, h_swish=True, include_top=True).to("cpu")
+        pn = PhiNet(res=32, B0=9, alpha=0.25, beta=1.3, t_zero=6, squeeze_excite=True, h_swish=True, include_top=True).to("cpu").eval()    
         
-        rand_x = Variable(torch.Tensor(np.random.rand(4, 3, 32, 32))).to("cpu")
+        rand_x = Variable(torch.Tensor(np.random.rand(1, 3, 32, 32))).to("cpu")
 
         print(pn.forward(rand_x).shape)
         summary(pn, input_data=rand_x, col_names = ["input_size", "output_size", "num_params", "kernel_size", "mult_adds"], verbose=1, device="cpu")
--- examples/cifar10.py
+++ examples/cifar10.py
@@ -22,10 +22,10 @@ class PNCifar10(pl.LightningModule):
 
         self.model = PhiNet(
             res=32,
-            alpha=0.15,
-            B0=7,
-            beta=1,
-            t_zero=5,
+            alpha=0.25,
+            B0=9,
+            beta=1.3,
+            t_zero=6,
             squeeze_excite=True, 
             h_swish=True, 
             include_top=True
@@ -50,7 +50,10 @@ class PNCifar10(pl.LightningModule):
         label = y.view(-1)
         out = self(img)
         loss = self.loss_fn(out,label)
+        logits = torch.argmax(out,dim=1)
+        accu = self.accuracy(logits, label)
         
+        self.log('train_acc', accu)
         self.log('train_loss', loss)
 
         return loss       
@@ -79,17 +82,24 @@ class Cifar10Dataset(pl.LightningDataModule):
                                                 train=True, 
                                                 download=True, 
                                                 transform=transforms.ToTensor())
+
+        self.test_set = torchvision.datasets.CIFAR10(data_path,
+                                                train=False, 
+                                                download=True, 
+                                                transform=transforms.ToTensor())
     
     def setup(self, stage=None):
-        self.train_set, self.val_set = train_test_split(self.root_set,
-                                                        test_size = 0.3)
+        self.train_set, self.val_set = train_test_split(self.root_set, test_size = 0.3)
 
     def train_dataloader(self, stage=None):
         train_loader = DataLoader(self.train_set, batch_size=256, num_workers=16)
 
-        return train_loader
+        return DataLoader(self.root_set, batch_size=256, num_workers=16)
+
+    # def val_dataloader(self):
+    #     valid_loader = DataLoader(self.val_set, batch_size=256, num_workers=16)
+
+    #     return valid_loader
 
     def val_dataloader(self):
-        valid_loader = DataLoader(self.val_set, batch_size=256, num_workers=16)
-
-        return valid_loader
+        return DataLoader(self.test_set, batch_size=256, num_workers=16)
