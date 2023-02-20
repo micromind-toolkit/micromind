9d3fa6ab316de3db3ccc273f261a52a88d7593ce
Matteo Beltrami <beltramimatteo01@gmail.com>
2023-02-20 09:48:31 +0100

    update datasets_info with MNIST

--- phinet/__init__.py
+++ phinet/__init__.py
@@ -11,5 +11,5 @@ datasets_info = {
     "CIFAR-100": {"Nclasses": 100, "NChannels": 3, "ext": ".pth.tar"},
     "CIFAR-10": {"Nclasses": 10, "NChannels": 3, "ext": ".pth.tar"},
     "ImageNet-1k": {"Nclasses": 1000, "NChannels": 3, "ext": ".pth.tar"},
-    "MNIST": {"Nclasses": 10, "NChannels": 1, "ext": ".pth.tar"}
+    "MNIST": {"Nclasses": 10, "NChannels": 1, "ext": ".pth.tar"},
 }
