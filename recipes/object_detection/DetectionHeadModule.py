ce7b0579524af81d8e000139a4725a658483267d
Sebastian <sebastian.cavada.lab@gmail.com>
2023-06-07 12:50:40 +0200

    test_1 working

--- recipes/object_detection/DetectionHeadModule.py
+++ recipes/object_detection/DetectionHeadModule.py
@@ -1,5 +1,9 @@
 # Ultralytics YOLO custom model DetectionHeadModule.py
 
+import contextlib
+from copy import deepcopy
+from pathlib import Path
+
 import torch
 import torch.nn as nn
 
@@ -12,16 +16,26 @@ from ultralytics.nn.modules import (AIFI, C1, C2, C3, C3TR, SPP, SPPF, Bottlenec
                                     Classify, Concat, Conv, Conv2, ConvTranspose, Detect, DWConv, DWConvTranspose2d,
                                     Focus, GhostBottleneck, GhostConv, HGBlock, HGStem, Pose, RepC3, RepConv,
                                     RTDETRDecoder, Segment)
-from ultralytics.yolo.utils.torch_utils import (initialize_weights)
+from ultralytics.yolo.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, colorstr, emojis, yaml_load
+from ultralytics.yolo.utils.checks import check_requirements, check_suffix, check_yaml
+from ultralytics.yolo.utils.torch_utils import (fuse_conv_and_bn, fuse_deconv_and_bn, initialize_weights,
+                                                intersect_dicts, make_divisible, model_info, scale_img, time_sync)
+
+try:
+    import thop
+except ImportError:
+    thop = None
 
 class DetectionHeadModel(DetectionModel):
     """YOLOv8 detection model."""
 
-    def __init__(self, ch=3, nc=None, verbose=True):  # model, input channels, number of classes
-        super().__init__()
+    def __init__(self, cfg='yolov8n.yaml', ch=3, nc=None, verbose=True):  # model, input channels, number of classes
+        
+        super(DetectionModel, self).__init__()
+        self.yaml = cfg if isinstance(cfg, dict) else yaml_model_load(cfg)  # cfg dict
         
         # Read custom hardcoded model
-        self.model, self.save = parse_model_custom(ch=ch, verbose=verbose)  # model, savelist
+        self.model, self.save = parse_model_custom(nc, ch=ch, verbose=verbose)  # model, savelist
         self.names = {i: f'{i}' for i in range(self.yaml['nc'])}  # default names dict
         self.inplace = self.yaml.get('inplace', True)
 
@@ -51,7 +65,42 @@ def get_output_dim(data_config, model):
         names.append(layer.__class__)
     return [list(o.shape)[1:] for o in out_dim], names
 
-def parse_model_custom(ch, verbose=True):
+def yaml_model_load(path):
+    """Load a YOLOv8 model from a YAML file."""
+    import re
+
+    path = Path(path)
+    if path.stem in (f'yolov{d}{x}6' for x in 'nsmlx' for d in (5, 8)):
+        new_stem = re.sub(r'(\d+)([nslmx])6(.+)?$', r'\1\2-p6\3', path.stem)
+        LOGGER.warning(f'WARNING ⚠️ Ultralytics YOLO P6 models now use -p6 suffix. Renaming {path.stem} to {new_stem}.')
+        path = path.with_stem(new_stem)
+
+    unified_path = re.sub(r'(\d+)([nslmx])(.+)?$', r'\1\3', str(path))  # i.e. yolov8x.yaml -> yolov8.yaml
+    yaml_file = check_yaml(unified_path, hard=False) or check_yaml(path)
+    d = yaml_load(yaml_file)  # model dict
+    d['scale'] = guess_model_scale(path)
+    d['yaml_file'] = str(path)
+    return d
+
+def guess_model_scale(model_path):
+    """
+    Takes a path to a YOLO model's YAML file as input and extracts the size character of the model's scale.
+    The function uses regular expression matching to find the pattern of the model scale in the YAML file name,
+    which is denoted by n, s, m, l, or x. The function returns the size character of the model scale as a string.
+
+    Args:
+        model_path (str) or (Path): The path to the YOLO model's YAML file.
+
+    Returns:
+        (str): The size character of the model's scale, which can be n, s, m, l, or x.
+    """
+    with contextlib.suppress(AttributeError):
+        import re
+        return re.search(r'yolov\d+([nslmx])', Path(model_path).stem).group(1)  # n, s, m, l, or x
+    return ''
+
+
+def parse_model_custom(nc, ch, verbose=True):
 
     if verbose:
         LOGGER.info(f"\n{'':>3}{'from':>20}{'n':>3}{'params':>10}  {'module':<45}{'arguments':<30}")
@@ -106,71 +155,73 @@ def parse_model_custom(ch, verbose=True):
     layer9 = SPPF(256,256, 5)
     layer9.i, layer9.f, layer9.type = 9, -1, 'ultralytics.nn.modules.block.SPPF'
     layers.append(layer9)
-    save.extend(x % i for x in ([layer9.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    save.extend(x % layer9.i for x in ([layer9.f] if isinstance(layer9.f, int) else layer9.f) if x != -1)  # append to savelist
 
     layer10 = nn.Upsample(scale_factor=2, mode='nearest')
     layer10.i, layer10.f, layer10.type = 10, -1, 'torch.nn.modules.upsampling.Upsample'
     layers.append(layer10)
-    save.extend(x % i for x in ([layer10.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    save.extend(x % layer10.i for x in ([layer10.f] if isinstance(layer10.f, int) else layer10.f) if x != -1)  # append to savelist
 
-    layer11 = Concat(layers[-1], layers[6], 1)
+    #layer11 = Concat((layers[-1], layers[6]))
+    layer11 = Concat(1)
     layer11.i, layer11.f, layer11.type = 11, [-1,6], 'ultralytics.nn.modules.conv.Concat'
-    layers.append(layer11)
-    save.extend(x % i for x in ([layer11.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    c2 = sum(ch[x] for x in layer11.f)
+    save.extend(x % layer11.i for x in ([layer11.f] if isinstance(layer11.f, int) else layer11.f) if x != -1)  # append to savelist
+    layers.append(layer11)    
 
     layer12 = C2f(384, 128, 1)
     layer12.i, layer12.f, layer12.type = 12, -1, 'ultralytics.nn.modules.block.C2f'
     layers.append(layer12)
-    save.extend(x % i for x in ([layer12.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    save.extend(x % layer12.i for x in ([layer12.f] if isinstance(layer12.f, int) else layer12.f) if x != -1)  # append to savelist
 
     layer13 = nn.Upsample(scale_factor=2, mode='nearest')
     layer13.i, layer13.f, layer13.type = 13, -1, 'torch.nn.modules.upsampling.Upsample'
     layers.append(layer13)   
-    save.extend(x % i for x in ([layer13.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    save.extend(x % layer13.i for x in ([layer13.f] if isinstance(layer13.f, int) else layer13.f) if x != -1)  # append to savelist
 
-    layer14 = Concat(layers[-1], layers[4], 1)
+    layer14 = Concat(1)
     layer14.i, layer14.f, layer14.type = 14, [-1,4], 'ultralytics.nn.modules.conv.Concat'
     layers.append(layer14)
-    save.extend(x % i for x in ([layer14.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    save.extend(x % layer14.i for x in ([layer14.f] if isinstance(layer14.f, int) else layer14.f) if x != -1)  # append to savelist
 
     layer15 = C2f(192, 64, 1)
     layer15.i, layer15.f, layer15.type = 15, -1, 'ultralytics.nn.modules.block.C2f'
     layers.append(layer15)
-    save.extend(x % i for x in ([layer15.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    save.extend(x % layer15.i for x in ([layer15.f] if isinstance(layer15.f, int) else layer15.f) if x != -1)  # append to savelist
 
     layer16 = Conv(64,64,3,2)
     layer16.i, layer16.f, layer16.type = 16, -1, 'ultralytics.nn.modules.conv.Conv'
     layers.append(layer16)
-    save.extend(x % i for x in ([layer16.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    save.extend(x % layer16.i for x in ([layer16.f] if isinstance(layer16.f, int) else layer16.f) if x != -1)  # append to savelist
 
-    layer17 = Concat(layers[-1], layers[12], 1)
+    layer17 = Concat(1)
     layer17.i, layer17.f, layer17.type = 17, [-1,12], 'ultralytics.nn.modules.conv.Concat'
     layers.append(layer17)
-    save.extend(x % i for x in ([layer17.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    save.extend(x % layer17.i for x in ([layer17.f] if isinstance(layer17.f, int) else layer17.f) if x != -1)  # append to savelist
 
     layer18 = C2f(192, 128, 1)
     layer18.i, layer18.f, layer18.type = 18, -1, 'ultralytics.nn.modules.block.C2f'
     layers.append(layer18)
-    save.extend(x % i for x in ([layer18.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    save.extend(x % layer18.i for x in ([layer18.f] if isinstance(layer18.f, int) else layer18.f) if x != -1)  # append to savelist
     
     layer19 = Conv(128,128,3,2)
     layer19.i, layer19.f, layer19.type = 19, -1, 'ultralytics.nn.modules.conv.Conv'
     layers.append(layer19)
-    save.extend(x % i for x in ([layer19.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    save.extend(x % layer19.i for x in ([layer19.f] if isinstance(layer19.f, int) else layer19.f) if x != -1)  # append to savelist
 
-    layer20 = Concat(layers[-1], layers[8], 1)
+    layer20 = Concat(1)
     layer20.i, layer20.f, layer20.type = 20, [-1,8], 'ultralytics.nn.modules.conv.Concat'
     layers.append(layer20)
-    save.extend(x % i for x in ([layer20.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    save.extend(x % layer20.i for x in ([layer20.f] if isinstance(layer20.f, int) else layer20.f) if x != -1)  # append to savelist
 
     layer21 = C2f(384, 256, 1)
     layer21.i, layer21.f, layer21.type = 21, -1, 'ultralytics.nn.modules.block.C2f'
     layers.append(layer21)
-    save.extend(x % i for x in ([layer21.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    save.extend(x % layer21.i for x in ([layer21.f] if isinstance(layer21.f, int) else layer21.f) if x != -1)  # append to savelist
 
     head = Detect(80, [64,128,256])
-    head.i, head.f, head.type = 22, [18,20,22], 'ultralytics.nn.modules.conv.Detect'
-    save.extend(x % i for x in ([head.f] if isinstance(f, int) else f) if x != -1)  # append to savelist
+    head.i, head.f, head.type = 22, [15,18,21], 'ultralytics.nn.modules.conv.Detect'
+    save.extend(x % head.i for x in ([head.f] if isinstance(head.f, int) else head.f) if x != -1)  # append to savelist
 
     layers.append(head)    
 
---
+++ recipes/object_detection/train.py
@@ -0,0 +1,38 @@
+# Ultralytics YOLO 🚀, AGPL-3.0 license
+from copy import copy
+
+import numpy as np
+
+from ultralytics.nn.tasks import DetectionModel
+from ultralytics.yolo import v8
+from ultralytics.yolo.data import build_dataloader, build_yolo_dataset
+from ultralytics.yolo.data.dataloaders.v5loader import create_dataloader
+from ultralytics.yolo.engine.trainer import BaseTrainer
+from ultralytics.yolo.utils import DEFAULT_CFG, LOGGER, RANK
+
+from ultralytics.yolo.v8.detect import DetectionTrainer
+from DetectionHeadModule import DetectionHeadModel
+
+
+# BaseTrainer python usage
+class DetectionMicroTrainer(DetectionTrainer):
+
+    def get_model(self, cfg=None, weights=None, verbose=True):
+        """Return a YOLO detection model."""
+        model = DetectionHeadModel(nc=self.data['nc'], verbose=verbose and RANK == -1)
+        if weights:
+            model.load(weights)
+        return model   
+
+def train(cfg=DEFAULT_CFG, use_python=False):
+    """Train and optimize YOLO model given training data and device."""
+    model = cfg.model or 'yolov8n.pt'
+    data = cfg.data or 'coco128.yaml'  # or yolo.ClassificationDataset("mnist")
+    device = cfg.device if cfg.device is not None else ''
+
+    args = dict(model=model, data=data, device=device)   
+    trainer = DetectionMicroTrainer(overrides=args)
+    trainer.train()
+
+if __name__ == '__main__':
+    train()
