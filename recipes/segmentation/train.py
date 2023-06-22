14e7314a29b55ddd3a1658d80e2d4cdb13ddaea1
SebastianCavada <sebastian.cavada.lab@gmail.com>
2023-06-22 10:28:46 +0200

    small improvements to relative dataset import

--- recipes/segmentation/train.py
+++ recipes/segmentation/train.py
@@ -1,3 +1,5 @@
+import os
+
 from micromind import PhiNet
 
 from yolo.model import microYOLO
@@ -25,10 +27,17 @@ def train_nn():
         backbone=backbone, head=head, task="segment", nc=80
     )  # build a new model from scratch DEFAULT_CFG
 
-    # Train the model
-    model.train(data="coco128-seg.yaml", epochs=50, imgsz=320, device=0, batch=4)
-    model.export()
+    # Train the model            
+    model.train(data=get_dataset("coco-seg.yaml"), epochs=50, imgsz=320, device="cpu", batch=4)
+    model.export()    
 
+def get_dataset(dataset_name):
+    cwd = os.getcwd()
+    dataset_path = os.path.join(cwd, "datasets")
+    if not os.path.exists(dataset_path):
+        raise Exception("Dataset not found")
+    else:
+        return os.path.join(dataset_path, dataset_name)    
 
 if __name__ == "__main__":
     train_nn()
