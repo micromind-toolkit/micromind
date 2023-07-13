977e039b64bc9ebd2bd2a5dc1b8ea6ef01650b4f
SebastianCavada <sebastian.cavada.lab@gmail.com>
2023-07-13 15:17:39 +0200

    new_start

--- recipes/detection/detection.py
+++ recipes/detection/detection.py
@@ -6,31 +6,41 @@ from yolo.microyoloheadphiblock import Microhead
 
 def train_nn():
 
-    _alpha = 0.33
+    _alpha = 0.67
+    _deeper_head = False
 
+    if(_deeper_head):
+        _feature_sizes = [
+            int(32 * _alpha / 0.67),
+            int(64 * _alpha / 0.67),
+            int(128 * _alpha / 0.67)
+        ]
+    else:
+        _feature_sizes = [
+            int(16 * _alpha / 0.67),
+            int(32 * _alpha / 0.67),
+            int(64 * _alpha / 0.67),            
+        ]
+        
     # define backbone
     backbone = PhiNet(
         input_shape=(3, 320, 320),
         alpha=_alpha,
-        num_layers=7,
+        num_layers=6,
         beta=1,
         t_zero=4,
         include_top=False,
         num_classes=80,
         compatibility=True,
-        downsampling_layers=[5, 7, 8],  # S2
+        downsampling_layers=[5, 7],  # S2
         squeeze_excite=False,
     )
     # define head
     head = Microhead(
-        feature_sizes=[
-            int(32 * _alpha / 0.67),
-            int(64 * _alpha / 0.67),
-            int(128 * _alpha / 0.67),
-        ],
+        feature_sizes=_feature_sizes,
         concat_layers=[6, 4, 12, 9],
         head_concat_layers=[15, 18, 21],
-        deeper_head=True,
+        deeper_head=_deeper_head,
         no_SPPF=False,
     )
 
