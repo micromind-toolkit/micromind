4b0fc47f93c8092eb37121782eafe5a841e6cc49
Sebastian <sebastian.cavada.lab@gmail.com>
2023-07-17 11:56:08 +0200

    update the plotting and benching

--- recipes/detection/bench.py
+++ recipes/detection/bench.py
@@ -1,11 +1,23 @@
+import os
+import shutil
+
 from micromind import benchmark
+
+weight = "_new_start_yms_1_head_alpha067"
 
 # Benchmark on GPU
 benchmark(
-    model="./runs/detect/train36/weights/best.pt",
+    model="./benchmark/weights/" + weight + "/weights/best.pt",
     imgsz=320,
     half=True,
     device="cpu",
 )  # microyolo with phinet
+
+# rename benchmark.log to weight.log
+os.rename("benchmarks.log", weight + ".log")
+
+# move to benchmark folder
+shutil.move(weight + ".log", "./benchmark/plots/data/optimized/" + weight + ".log")
+
 # benchmark(model='yolov8n.pt', imgsz=320, half=False, device='cpu') # yolov8 nano
 # benchmark(model="yolov8s.pt", imgsz=320, half=True, device="cpu")  # yolov8 small
