898019bad975aa6fcba73c71273d765bf192a430
Sebastian <sebastian.cavada.lab@gmail.com>
2023-07-19 09:26:51 +0200

    small changes

--- micromind/yolo/__init__.py
+++ micromind/yolo/__init__.py
@@ -2,7 +2,7 @@ from .model import microYOLO
 
 from .microyoloheadphiblock import Microhead
 
-from .benchmarks import benchmark
+from .utils.benchmarks import benchmark
 
 from .detection.detectionmicromodule import DetectionMicroModel
 from .detection.detectionmicrotrainer import DetectionMicroTrainer
