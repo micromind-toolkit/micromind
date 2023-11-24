db6963fa3b49946b6984f9c2635cb4bca515a748
fpaissan <me@francescopaissan.it>
2023-11-24 10:00:41 +0100

    add docstrings to prepare_data

--- recipes/objection_detection/prepare_data.py
+++ recipes/objection_detection/prepare_data.py
@@ -6,7 +6,18 @@ from ultralytics.data import build_yolo_dataset
 
 def create_loaders(m_cfg: Dict, data_cfg: Dict, batch_size: int):
     """Creates DataLoaders for dataset specified in the configuration file.
-    Refer to ... for how to select the proper configuration."""
+    Refer to ... for how to select the proper configuration.
+
+    Arguments
+    ---------
+    m_cfg : Dict
+        Contains information about the training process (e.g., data augmentation).
+    data_cfg : Dict
+        Contains details about the data configurations (e.g., image size, etc.).
+    batch_size : int
+        Batch size for the training process.
+
+    """
     mode = "train"
     train_set = build_yolo_dataset(
         m_cfg,
